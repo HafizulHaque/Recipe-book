548be477a725b1853741de0119992c132377205f
Hafizul Haque <hafizul.haque@reddotdigitalit.com>
2022-02-03 02:38:05 +0600

    Recipe items page template portion implemented

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,7 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe(
+      'A test Recipe',
+      'This is simply a test maybe',
+      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'
+    ),
+    new Recipe(
+      'Second Recipe of the day',
+      'This is really awesome, spicy and juicy',
+      'https://cdn.pixabay.com/photo/2021/12/30/11/33/italian-cuisine-6903774_960_720.jpg'
+    ),
+  ];
 
   constructor() { }
 
