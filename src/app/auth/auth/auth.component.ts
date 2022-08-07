0b2c58ffda81bcba0b214f515f9ce2fd2d1fa8f9
Hafizul Haque <hafizul.haque@reddotdigitalit.com>
2022-08-07 17:39:36 +0600

    end of chapter 22 (angular modules & optimizations)

--- src/app/auth/auth/auth.component.ts
+++ src/app/auth/auth/auth.component.ts
@@ -1,5 +1,3 @@
-import { HttpErrorResponse } from '@angular/common/http';
-import { Placeholder } from '@angular/compiler/src/i18n/i18n_ast';
 import { Component, ComponentFactoryResolver, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
